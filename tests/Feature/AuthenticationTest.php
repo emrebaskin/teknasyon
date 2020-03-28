5ded0383d71a285040aacb9208d2d7c3041e0dda
Emre Baskın <emre.bskn@gmail.com>
2020-03-28 22:25:05 +0300

    unittest implemented

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+
+    use WithFaker;
+
+    public $email = 'example@example.com';
+    public $password = '123456';
+
+    public function testRegister()
+    {
+
+        $userData = [
+            'name'                  => $this->faker->name,
+            'email'                 => $this->faker->unique()->safeEmail,
+            'password'              => '123456',
+            'password_confirmation' => '123456',
+        ];
+
+        $response = $this->post(route('api.register'), $userData);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'errorCode',
+            'errorMessage',
+            'result' => [
+                'user' => [
+                    'id',
+                    'name',
+                    'email',
+                ],
+            ],
+        ]);
+
+        $response->assertJsonFragment([
+            'errorCode'    => 0,
+            'errorMessage' => null,
+        ]);
+
+    }
+
+    public function testLogin()
+    {
+
+        $userData = [
+            'email'    => $this->email,
+            'password' => $this->password,
+        ];
+
+        $response = $this->post(route('api.login'), $userData);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'errorCode',
+            'errorMessage',
+            'result' => [
+                'user' => [
+                    'id',
+                    'name',
+                    'email',
+                ],
+            ],
+        ]);
+
+        $response->assertJsonFragment([
+            'errorCode'    => 0,
+            'errorMessage' => null,
+        ]);
+
+    }
+
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,97 @@
 
 namespace Tests;
 
+use App\User;
+use DateTime;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\ClientRepository;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseMigrations;
+
+    protected $headers = [];
+    protected $scopes = [];
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $clientRepository = new ClientRepository();
+        $client           = $clientRepository->createPersonalAccessClient(
+            null, 'Test Personal Access Client', 'http://localhost'
+        );
+
+        DB::table('oauth_personal_access_clients')->insert([
+            'client_id'  => $client->id,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+
+        $this->user           = new User();
+        $this->user->name     = 'Test User';
+        $this->user->email    = 'example@example.com';
+        $this->user->password = '123456';
+        $this->user->save();
+
+        $token                          = $this->user->createToken('TestToken', $this->scopes)->accessToken;
+        $this->headers['Accept']        = 'application/json';
+        $this->headers['Authorization'] = 'Bearer '.$token;
+    }
+
+    public function get($uri, array $headers = [])
+    {
+        return parent::get($uri, array_merge($this->headers, $headers));
+    }
+
+    public function getJson($uri, array $headers = [])
+    {
+        return parent::getJson($uri, array_merge($this->headers, $headers));
+    }
+
+    public function post($uri, array $data = [], array $headers = [])
+    {
+        return parent::post($uri, $data, array_merge($this->headers, $headers));
+    }
+
+    public function postJson($uri, array $data = [], array $headers = [])
+    {
+        return parent::postJson($uri, $data, array_merge($this->headers, $headers));
+    }
+
+    public function put($uri, array $data = [], array $headers = [])
+    {
+        return parent::put($uri, $data, array_merge($this->headers, $headers));
+    }
+
+    public function putJson($uri, array $data = [], array $headers = [])
+    {
+        return parent::putJson($uri, $data, array_merge($this->headers, $headers));
+    }
+
+    public function patch($uri, array $data = [], array $headers = [])
+    {
+        return parent::patch($uri, $data, array_merge($this->headers, $headers));
+    }
+
+    public function patchJson($uri, array $data = [], array $headers = [])
+    {
+        return parent::patchJson($uri, $data, array_merge($this->headers, $headers));
+    }
+
+    public function delete($uri, array $data = [], array $headers = [])
+    {
+        return parent::delete($uri, $data, array_merge($this->headers, $headers));
+    }
+
+    public function deleteJson($uri, array $data = [], array $headers = [])
+    {
+        return parent::deleteJson($uri, $data, array_merge($this->headers, $headers));
+    }
+
 }
